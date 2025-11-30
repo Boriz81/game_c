56c41e787befcd9d1d0d81570bb2a419d84e1c07
Boriz81 <blitzboriz@gmail.com>
2025-11-30 22:07:13 +0900

    target correct 1

--- src/game.c
+++ src/game.c
@@ -14,8 +14,8 @@ bool game_running;
 void init_game() {
     player.x = MAP_WIDTH / 2;
     player.y = MAP_HEIGHT / 2;
-    target.x = 5;
-    target.y = 10;
+    target.x = MAP_WIDTH / 2 + 5;
+    target.y = MAP_HEIGHT / 2;
     game_running = true;
 }
 
@@ -28,41 +28,51 @@ void update_game() {
     timeout(-1);
     ch = getch();
     if (ch != ERR) {
-	switch(ch) {
-	    case 'w' : player.y--; break;
-	    case 's' : player.y++; break;
-	    case 'a' : player.x--; break;
-	    case 'd' : player.x++; break;
-	    case 'q' : game_running = false; break;
-	}
+	    switch(ch) {
+	        case 'w' : player.y--; break;
+	        case 's' : player.y++; break;
+	        case 'a' : player.x--; break;
+	        case 'd' : player.x++; break;
+	        case 'q' : game_running = false; break;
+	    }
 
 	// Проверка границ
-	if (player.x < 0) player.x = 0;
-	if (player.x >= MAP_WIDTH) player.x = MAP_WIDTH - 1;
-	if (player.y < 0) player.y = 0;
-	if (player.y >= MAP_HEIGHT) player.y = MAP_HEIGHT - 1;
+	    if (player.x < 0) player.x = 0;
+	    if (player.x >= MAP_WIDTH) player.x = MAP_WIDTH - 1;
+	    if (player.y < 0) player.y = 0;
+	    if (player.y >= MAP_HEIGHT) player.y = MAP_HEIGHT - 1;
     }
+
     endwin();
+    
 }
 
 void render_game() {
-    system("clear"); // Очистка экрана (Linux)
-    // Для виндовс system("cls")
+    system("clear");
     
-    // Рендеринг карты
+    // Верхняя граница
+    for (int x = 0; x < MAP_WIDTH + 2; x++) printf("#");
+    printf("\n");
     
+    // Поле с боковыми границами
     for (int y = 0; y < MAP_HEIGHT; y++) {
-	for (int x = 0; x < MAP_WIDTH; x++) {
-        if (x == target.x && y == target.y) printf("0");
-	    if (x == player.x && y == player.y) {
-		printf("@"); //игрок
-	    } else {
-	        printf(" ");
-	    }
-	}
-	printf("\n");
+        printf("#"); // левая граница
+        for (int x = 0; x < MAP_WIDTH; x++) {
+            if (x == player.x && y == player.y) {
+                printf("@");
+            } else if (x == target.x && y == target.y) {
+                printf("0");
+            } else {
+                printf(" ");
+            }
+        }
+        printf("#\n"); // правая граница
     }
-
+    
+    // Нижняя граница
+    for (int x = 0; x < MAP_WIDTH + 2; x++) printf("#");
+    printf("\n");
+    
     printf("Controls: WASD to move, Q to quit\n");
 }
 
